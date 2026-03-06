8061af55a1d87c051086a7d6673184e928232aab
Ikhlas-El <ikhlaselissaoui6@gmail.com>
2026-03-06 10:08:01 +0100

    Remove Powered by Odoo from receipt

---
+++ pos_remove_powered_by/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "POS Remove Powered By",
+    "version": "1.0",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_remove_powered_by/static/src/xml/order_receipt.xml",
+        ],
+    },
+    "installable": True,
+}
